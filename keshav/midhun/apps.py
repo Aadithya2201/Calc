8ab5b907c93daa505375be4c5feb4ee01de890d5
Aadithya2201 <aadithya01jan@gmail.com>
2024-04-26 20:02:23 +0530

    Success

---
+++ keshav/midhun/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MidhunConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'midhun'
---
+++ keshav/midhun/static/working.js
@@ -0,0 +1,20 @@
+function percent() {
+    x = document.getElementById("result").value;
+    y = eval(x);
+    percentage = y / 100;
+    document.getElementById("result").value = percentage;
+}
+function solve() {
+    x = document.getElementById("result").value;
+    y = eval(x);
+    document.getElementById("result").value = y;
+}
+
+function clr() {
+    document.getElementById("result").value = "";
+}
+function dis(val) {
+    document.getElementById("result").value += val;
+}
+
+
